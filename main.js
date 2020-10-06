8020ce944f11a41b6aa87c4843695ace9ade90cf
Oliver Dimes <odime001@gold.ac.uk>
2020-10-06 18:08:26 +0100

    Alert system in place for login for API requests.

--- main.js
+++ main.js
@@ -1,234 +1,212 @@
 //API Request Test
+//Login Details are "testuser1@edocuments.co.uk","20DemoPass20"
+
 var tasks;
 var portfolio;
-//Retrieving Username and Password information from the web page
-
 toggleTables('tasks');
-//To retrieve the user token, returns user token
-async function getUserToken(username, password)
-{
-    try{
-        
-        let response = await fetch("https://edocsapi.azurewebsites.net/Auth/api/Login",{
-         method:'POST',
-         headers : {
-             'Content-Type' : 'application/json',
-             'Authorization' : `Digest username="xikxafatwae" realm="_root_" password=""` 
-         },
-         body : JSON.stringify({ "username" : username, "password" : password})
-         })
+
+//Retrieves the user token, returns user token
+async function getUserToken(username, password) {
+    try {
+        let response = await fetch("https://edocsapi.azurewebsites.net/Auth/api/Login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Digest username="xikxafatwae" realm="_root_" password=""`
+            },
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            })
+        })
         let data = await response.json();
         let tokenValue = await data.Result.auth.token;
         return tokenValue;
-    }
-    catch (err)
-    {
-        alert(err)
-        console.log(err)
+    } catch (err) {
+        alert(err);
+        console.log(err);
     }
 }
 
 //Function to check if the user token is valid, returns true or false.
-async function testToken(password){
-        try{
-        let formatedString = `Digest username="xikxafatwae" realm="_root_" password="${password}"`
-        
-        let response = await fetch("https://edocsapi.azurewebsites.net/Auth/api/Test",{
-         method:'POST',
-         headers : {
-             'Authorization' : formatedString
-         }
-         })
+async function testToken(password) {
+    try {
+        let fString = `Digest username="xikxafatwae" realm="_root_" password="${password}"`
+        let response = await fetch("https://edocsapi.azurewebsites.net/Auth/api/Test", {
+            method: 'POST',
+            headers: {
+                'Authorization': fString
+            }
+        })
         let data = await response.json();
-        console.log(data)
         let tokenValue = await data.Result.auth.token;
         return tokenValue;
-    }
-    catch (err)
-    {
-        alert("Username/Password Invalid")
-        console.log(err)
+    } catch (err) {
+        alert(err);
+        console.log(err);
     }
 }
 
 //Function to retrieve portfolio, returns data from Portfolio
-async function getUserPortfolio(password){
-    try
-        {
-    let formatedString = `Digest username="xikxafatwae" realm="_root_" password="${password}"`
-    let response = await fetch("https://edocsapi.azurewebsites.net/Core6/api/Portfolio/ByUserId",{
-     method:'GET',
-     headers : {
-         'Content-Type' : 'application/json',
-         'Authorization' : formatedString
-     }});
-    let data = await response.json();  
-    
-    portfolio = await data.Result.sites;
-    let temp = "";
-    
-    //For Body
-        //For loop to grab data and display it in a Bootstrap table
-        portfolio.forEach((u) =>{
-            temp += `<tr>`;
-            temp += "<td>"+u.name+"</td>";
-            temp += `<td>`+u.id+`</td>`;
-            temp += "<td>"+u.address+"</td>";
-            temp += "<td>"+u.postcode+"</td>";
-            temp += "<td>"+u.projects.length+"</td>";
-            temp += `</tr>`;
-        })
-            
-        document.getElementById("portfolioBody").innerHTML = temp;
-            
-        console.log(portfolio);
-            
-        }
-    catch (err)
-        {
-            console.log(err);
-        }
-}
-
-async function getReportProjects(password,projectid)
-{
+async function getUserPortfolio(password) {
     try {
         let formatedString = `Digest username="xikxafatwae" realm="_root_" password="${password}"`
-        let response = await fetch(`https://edocsapi.azurewebsites.net/Core6/api/Reports/ByProjectId/${projectid}`,{
-         method:'GET',
-         headers : {
-             'Content-Type' : 'application/json',
-             'Authorization' : formatedString
-         }});
-        let data = await response.json();  
-        console.log(data)
-        }
-    catch (err)
-        {
-            console.log(err);
-        }
-}
+        let response = await fetch("https://edocsapi.azurewebsites.net/Core6/api/Portfolio/ByUserId", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': formatedString
+            }
+        });
+        let data = await response.json();
 
-
-async function getTasksByUser(password){
-    try {
-        let formatedString = `Digest username="xikxafatwae" realm="_root_" password="${password}"`
-        let response = await fetch(`https://edocsapi.azurewebsites.net/Core6/api/Tasks/ByUser`,{
-         method:'GET',
-         headers : {
-             'Content-Type' : 'application/json',
-             'Authorization' : formatedString
-         }});
-        let data = await response.json();  
-        tasks = await data.Result.tasks;
-        
-        let tempHead = "";
+        portfolio = await data.Result.sites;
         let temp = "";
-        
+
         //For Body
         //For loop to grab data and display it in a Bootstrap table
-        tasks.forEach((u) =>{
+        portfolio.forEach((u) => {
             temp += `<tr>`;
-            temp += `<td>`+u.documentId+`</td>`
-            temp += "<td>"+u.subject+"</td>"
-            temp += "<td>"+u.createdDate+"</td>"
-            temp += "<td>"+u.dueDate+"</td>"
-            temp += "<td>"+u.taskMessages[0].body+"</td>"
+            temp += "<td>" + u.name + "</td>";
+            temp += `<td>` + u.id + `</td>`;
+            temp += "<td>" + u.address + "</td>";
+            temp += "<td>" + u.postcode + "</td>";
+            temp += "<td>" + u.projects.length + "</td>";
+            temp += `</tr>`;
+        })
+        document.getElementById("portfolioBody").innerHTML = temp;
+        console.log(portfolio);
+
+    } catch (err) {
+        alert(err);
+        console.log(err);
+    }
+}
+
+async function getReportProjects(password, projectid) {
+    try {
+        let formatedString = `Digest username="xikxafatwae" realm="_root_" password="${password}"`
+        let response = await fetch(`https://edocsapi.azurewebsites.net/Core6/api/Reports/ByProjectId/${projectid}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': formatedString
+            }
+        });
+        let data = await response.json();
+    } catch (err) {
+        alert(err);
+        console.log(err);
+    }
+}
+
+
+async function getTasksByUser(password) {
+    try {
+        let formatedString = `Digest username="xikxafatwae" realm="_root_" password="${password}"`
+        let response = await fetch(`https://edocsapi.azurewebsites.net/Core6/api/Tasks/ByUser`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': formatedString
+            }
+        });
+        let data = await response.json();
+        tasks = await data.Result.tasks;
+
+        let temp = "";
+
+        //For Body
+        //For loop to grab data and display it in a Bootstrap table
+        tasks.forEach((u) => {
+            temp += `<tr>`;
+            temp += `<td>` + u.documentId + `</td>`
+            temp += "<td>" + u.subject + "</td>"
+            temp += "<td>" + u.createdDate + "</td>"
+            temp += "<td>" + u.dueDate + "</td>"
+            temp += "<td>" + u.taskMessages[0].body + "</td>"
             temp += `</tr>`
         })
-        
         //Inserts data into table
-//        document.getElementById("tasksHead").innerHTML = tempHead;
         document.getElementById("tasksBody").innerHTML = temp;
-        
-        console.log(tasks)
-        
+    } catch (err) {
+        alert(err);
+        console.log(err);
+    }
 
-        }
-    catch (err)
-        {
-            console.log(err);
-        }
-    
 }
 
 
 //Based off https://www.w3schools.com/howto/howto_js_filter_table.asp
-function tableSearch(inputID,tableIndex,tableType){
+function tableSearch(inputID, tableIndex, tableType) {
     let input, filter, table, tr, td, i, txtValue;
     input = document.getElementById(inputID);
     filter = input.value.toUpperCase();
     table = document.getElementById(tableType);
     tr = table.getElementsByTagName("tr");
-    
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[tableIndex];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[tableIndex];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
     }
-  }
-    
+
 }
 
 //Shows and hides tables for searching
-function toggleTables(variable){
+function toggleTables(variable) {
     let x = document.getElementById("tasksTable");
     let y = document.getElementById("portfolioTable");
-    
-    switch(variable){
+
+    switch (variable) {
         case 'tasks':
-            console.log("Tasks is displayed");
             y.style.display = "none";
             x.style.display = "block";
             break;
         case 'portfolio':
-            console.log("Portfolio is displayed")
             x.style.display = "none";
             y.style.display = "block";
             break;
     }
-    
+
 }
 
 //Grabs user login value
-async function login(){
+async function login() {
     try {
-            let usernameVal = document.getElementById("username").value;
-            let passwordVal = document.getElementById("password").value;
-            
-            return {username : usernameVal, password: passwordVal}
-    } 
-    catch(err) {
-            console.log(err);
-    } 
+        let usernameVal = document.getElementById("username").value;
+        let passwordVal = document.getElementById("password").value;
 
-     
+        return {
+            username: usernameVal,
+            password: passwordVal
+        }
+    } catch (err) {
+        alert(err);
+        console.log(err);
+    }
+
+
 }
 
-async function main(){
-    
+async function main() {
     //Generates a user token that can be used
-    let loginDetails = await login()
-    let userToken = await getUserToken(loginDetails.username, loginDetails.password);
-    let testToken = await testToken(userToken);
-    let tasksByUser = await getTasksByUser(userToken);
-    let portfolioByUser = await getUserPortfolio(userToken);
+    try {
+        let loginDetails = await login()
+        let userToken = await getUserToken(loginDetails.username, loginDetails.password);
+        let tasksByUser = await getTasksByUser(userToken);
+        let portfolioByUser = await getUserPortfolio(userToken);
+    } catch (err) {
+        alert(err);
+        console.log(err);
+    }
+
 
 }
-
-//main().catch(console.log);
-
-
-//"testuser1@edocuments.co.uk","20DemoPass20"
-
-
-
-
-
-
